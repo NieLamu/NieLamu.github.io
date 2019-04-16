1c4d4a32e9b535f4019c17f17d28e1c69a6cb654
NieLamu <mxmxlty@gmail.com>
2019-04-16 17:36:37 +0800

    Add scripts

--- scripts/subrepos.sh
+++ scripts/subrepos.sh
@@ -3,4 +3,10 @@ trap "echo Exiting...;exit" SIGHUP SIGINT SIGQUIT SIGTERM
 subrepos_dir="subRepos"
 for name in $(ls -1 ${subrepos_dir});do
     echo 'Sub Repo: ' ${name}
+    cp source/_posts/${name}.md ${subrepos_dir}/${name}/README.md
+    cd ${subrepos_dir}/${name}/
+    git add .
+    git commit -m "$1"
+    git push
+    cd ../../
 done
--- source/_posts/2019-04-16-Awesome-GitHub-Repos.md
+++ source/_posts/2019-04-16-Awesome-GitHub-Repos.md
@@ -7,7 +7,14 @@ tags:
 date: 2019-04-16 15:37:07
 ---
 
+# Awesome GitHub Repos
 
+## Mobile UI framework/Kit
+
+- [framework7](https://github.com/framework7io/framework7/)
+
+
+## Linux Tools
 
 ----
 
